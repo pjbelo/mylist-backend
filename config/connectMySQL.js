497e7a79528bc4d70b2adbb138af519e6fd86030
Paulo Belo <pjbelo@gmail.com>
2020-07-09 21:22:34 +0100

    Production DB config

--- config/connectMySQL.js
+++ config/connectMySQL.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 module.exports = {
   con: mysql.createConnection({
-    host: "localhost",
-    user: "mylist",
+    host: "zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+    user: "p8x78gjar439uua1",
     password: process.env.DBPASS,
-    database: "mylist",
+    database: "x9w2k8xsqk7zy02x",
   }),
 };
